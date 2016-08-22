235385efac4461e86e74772e579fb2a59de7535e
jayaramyalla <jayaram.bugs@gmail.com>
2016-08-22 19:08:53 +0530

    Update vlangrabber.py

--- vlangrabber.py
+++ vlangrabber.py
@@ -1,18 +1,22 @@
 import commands
-p="ping 172.20.%s.1 -c 1"
+command="ping %s -c 1"
+ip="172.20.%s.1"
 w=open("output.txt","a")
 for i in range(255):
     try:
-        d = p % str(i)
-        print d
+		uip= ip % str(i)
+        d = command % uip
         pp=commands.getoutput(d)
-        print pp
         if "received, 0% packet loss," in pp:
-            pp=pp+"\n"
-            w.writelines(d)
+			uip=uip+"\n"
+            w.writelines(uip)
         else:
             pass
     except Exception:
         pass
 
 w.close()
+
+#JayaramYalla
+#https://twitter.com/jayaramyalla
+#https://in.linkedin.com/in/jayaramyalla
